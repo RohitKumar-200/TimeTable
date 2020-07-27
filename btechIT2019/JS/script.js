9060f013ec629c22d9a9e79fcf238ec33dbb06dc
rohitiiitl <lit2019037@iiitl.ac.in>
2020-07-27 21:28:23 +0530

    added info of four faculties

--- btechIT2019/JS/script.js
+++ btechIT2019/JS/script.js
@@ -14,3 +14,20 @@ setInterval(()=>{
     const dt = new Date();
     document.getElementById("date-time").innerHTML = day(dt.getDay()) +" "+ (("0"+dt.getDate()).slice(-2)) +"/"+ (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
 }, 1000);
+
+/* --------- Faculty info -------*/
+fetch('api/faculties.json').then(response => {
+    if(response.ok){
+        return response.json();
+    }
+    throw new Error('Request failed!');
+    }, networkError => {console.log(networkError.message);}
+    ).then(jsonResponse => {
+        console.log(jsonResponse);
+        const temp = document.getElementById('faculty-script').innerHTML;
+        const tempFun = Handlebars.compile(temp)   //this returns a function that takes object as argument
+        const html = tempFun({
+            'faculty-array': jsonResponse
+        });
+        document.getElementById('faculty-info-container').innerHTML = html;
+    });
